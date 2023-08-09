091a8a34e6bb4a6caed755bfebbf3ee3ed8fbe0d
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-09 19:11:51 +0400

    add new codes navigation and thumbs

--- src/component/image-slider/ProImageSlider.jsx
+++ src/component/image-slider/ProImageSlider.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './ProImageSlider.scss';
-
+import { Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Thumbs} from 'swiper/modules';
 
 const ImageSliderPro = props => {
   return (
-    <div>ImageSliderPro</div>
+    <>
+      <Swiper
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation, Thumbs]}
+        grabCursor={true}
+        className='pro-img-slide'
+      >
+        {
+          props.images.map((item, index) => (
+            <SwiperSlide key={index}>
+              <img src={item} alt="pro-img" />
+
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </>
   )
 }
 
 ImageSliderPro.propTypes = {
-    images: PropTypes.array.isRequired
+  images: PropTypes.array.isRequired
 }
 
 export default ImageSliderPro;
