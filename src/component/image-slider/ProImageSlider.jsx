6643f342911c6dfaeddd1b697ef971b496bff9bd
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-09 19:47:29 +0400

    add new codes

--- src/component/image-slider/ProImageSlider.jsx
+++ src/component/image-slider/ProImageSlider.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './ProImageSlider.scss';
-import { Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Thumbs} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Thumbs } from 'swiper/modules';
 
 const ImageSliderPro = props => {
+  // const [activeThumb, setActiveThumb] = useState();
+
+
   return (
     <>
       <Swiper
@@ -13,6 +16,7 @@ const ImageSliderPro = props => {
         navigation={true}
         modules={[Navigation, Thumbs]}
         grabCursor={true}
+        // thumbs={{swiper:activeThumb}}
         className='pro-img-slide'
       >
         {
@@ -20,6 +24,25 @@ const ImageSliderPro = props => {
             <SwiperSlide key={index}>
               <img src={item} alt="pro-img" />
 
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+
+      <Swiper
+        // onSwiper={setActiveThumb}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[Navigation, Thumbs]}
+        className='pro-img-slide-thumbs'
+      >
+        {
+          props.images.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="pro-img-slide-thumbs-wrapper">
+                <img src={item} alt="pro-img" />
+              </div>
             </SwiperSlide>
           ))
         }
